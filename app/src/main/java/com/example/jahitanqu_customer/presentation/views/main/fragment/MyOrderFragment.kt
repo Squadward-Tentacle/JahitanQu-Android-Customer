149481c3874e5646befbe2fccbf5e3acdeaf1d01
maulana <maulana@mymindstores.com>
2020-09-05 00:50:29 +0700

    [ADD] view pager for layout my order

--- app/src/main/java/com/example/jahitanqu_customer/presentation/views/main/fragment/MyOrderFragment.kt
+++ app/src/main/java/com/example/jahitanqu_customer/presentation/views/main/fragment/MyOrderFragment.kt
@@ -1,12 +1,17 @@
 package com.example.jahitanqu_customer.presentation.views.main.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.jahitanqu_customer.R
+import kotlinx.android.synthetic.main.fragment_my_order.*
+
 
 class MyOrderFragment : Fragment() {
 
@@ -20,6 +25,43 @@ class MyOrderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_order, container, false)
+        val view = inflater.inflate(R.layout.fragment_my_order, container, false)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tab_layout.setupWithViewPager(view_pager, true)
+        val viewPagerAdapter = ViewPagerAdapter(childFragmentManager, 1)
+        viewPagerAdapter.addFragment(MyOrderActiveFragment(), "Active")
+        viewPagerAdapter.addFragment(MyOrderHistoryFragment(), "History")
+        view_pager.adapter = viewPagerAdapter
+    }
+
+    private class ViewPagerAdapter(fm: FragmentManager, behavior: Int) :
+        FragmentPagerAdapter(fm, behavior) {
+
+        val listFragment = mutableListOf<Fragment>()
+        val listString = mutableListOf<String>()
+
+        fun addFragment(fragment: Fragment, title: String) {
+            listFragment.add(fragment)
+            listString.add(title)
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return listFragment[position]
+        }
+
+        override fun getCount(): Int {
+            return listFragment.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return listString[position]
+
+        }
+
     }
 }
